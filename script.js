628dae22dfd031cf1d31e9f3d9449e54c3160b0b
pinguineitor09 <crisguevara.v@gmail.com>
2024-10-18 23:40:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const totalCards = 16;
 const availableCards = [
-   'im1.jpg', 'im2.jpg', 'im3.jpg', 'im4.jpg', 'im5.jpg', 'im6.jpg', 'im7.jpg', 'im8.jpg',
-   'im9.jpg', 'im10.jpg', 'im11.jpg', 'im12.jpg', 'im13.jpg', 'im14.jpg', 'im15.jpg', 'im16.jpg'
+  'im1.jpg', 'im2.jpg', 'im3.jpg', 'im4.jpg', 'im5.jpg', 'im6.jpg', 'im7.jpg', 'im8.jpg',
+  'im9.jpg', 'im10.jpg', 'im11.jpg', 'im12.jpg', 'im13.jpg', 'im14.jpg', 'im15.jpg', 'im16.jpg'
 ];
 let cards = [];
 let selectedCards = [];
@@ -9,86 +9,84 @@ let currentMove = 0;
 let currentAttempts = 0;
 
 let cardTemplate = `
-   <div class="card">
-      <div class="back"></div>
-      <div class="face"></div>
-   </div>
+  <div class="card">
+    <div class="back"></div>
+    <div class="face"></div>
+  </div>
 `;
 
 function activate(e) {
-   if (currentMove < 2) {
-      if ((!selectedCards[0] || selectedCards[0] !== e.target) && !e.target.classList.contains('active')) {
-         e.target.classList.add('active');
-         selectedCards.push(e.target);
+  if (currentMove < 2) {
+    if ((!selectedCards[0] || selectedCards[0] !== e.target) && !e.target.classList.contains('active')) {
+      e.target.classList.add('active');
+      selectedCards.push(e.target);
 
-         if (++currentMove === 2) {
-            currentAttempts++;
-            document.querySelector('#stats').innerHTML = currentAttempts + ' intentos';
+      if (++currentMove === 2) {
+        currentAttempts++;
+        document.querySelector('#stats').innerHTML = currentAttempts + ' intentos';
 
-            const card1Value = selectedCards[0].dataset.value;
-            const card2Value = selectedCards[1].dataset.value;
+        const card1Value = selectedCards[0].dataset.value;
+        const card2Value = selectedCards[1].dataset.value;
 
-            if (Math.abs(card1Value - card2Value) === 1 && Math.min(card1Value, card2Value) % 2 !== 0) {
-               selectedCards[0].classList.add('matched');
-               selectedCards[1].classList.add('matched');
-               selectedCards = [];
-               currentMove = 0;
-            } else {
-               setTimeout(() => {
-                  selectedCards[0].classList.remove('active');
-                  selectedCards[1].classList.remove('active');
-                  selectedCards = [];
-                  currentMove = 0;
-               }, 600);
-            }
-         }
+        if (Math.abs(card1Value - card2Value) === 1 && Math.min(card1Value, card2Value) % 2 !== 0) {
+          selectedCards[0].classList.add('matched');
+          selectedCards[1].classList.add('matched');
+          selectedCards = [];
+          currentMove = 0;
+        } else {
+          setTimeout(() => {
+            selectedCards[0].classList.remove('active');
+            selectedCards[1].classList.remove('active');
+            selectedCards = [];
+            currentMove = 0;
+          }, 600);
+        }
       }
-   }
+    }
+  }
 }
 
 function shuffle(array) {
-   for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-   }
-   return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 function initializeGame() {
-   shuffle(availableCards);
-   for (let i = 0; i < totalCards; i++) {
-      let div = document.createElement('div');
-      div.innerHTML = cardTemplate;
-      cards.push(div);
-      document.querySelector('#game').append(cards[i]);
+  shuffle(availableCards);
+  for (let i = 0; i < totalCards; i++) {
+    let div = document.createElement('div');
+    div.innerHTML = cardTemplate;
+    cards.push(div);
+    document.querySelector('#game').append(cards[i]);
 
-      let face = cards[i].querySelectorAll('.face')[0];
-      face.style.backgroundImage = `url(images/${availableCards[i]})`;
-      face.style.backgroundSize = 'cover';
+    let face = cards[i].querySelectorAll('.face')[0];
+    face.style.backgroundImage = `url(images/${availableCards[i]})`;
+    face.style.backgroundSize = 'cover';
 
-      cards[i].querySelectorAll('.card')[0].dataset.value = availableCards[i].match(/\d+/)[0];
+    cards[i].querySelectorAll('.card')[0].dataset.value = availableCards[i].match(/\d+/)[0];
 
-      cards[i].querySelectorAll('.card')[0].addEventListener('click', activate);
-   }
+    cards[i].querySelectorAll('.card')[0].addEventListener('click', activate);
+  }
 }
 
 function resetGame() {
-   currentAttempts = 0;
-   document.querySelector('#stats').innerHTML = '0 intentos';
-   selectedCards = [];
-   currentMove = 0;
+  currentAttempts = 0;
+  document.querySelector('#stats').innerHTML = '0 intentos';
+  selectedCards = [];
+  currentMove = 0;
 
-   cards.forEach(card => {
-      card.classList.remove('active', 'matched');
-   });
+  cards.forEach(card => {
+    card.classList.remove('active', 'matched');
+  });
 
-   document.querySelector('#game').innerHTML = '';
-   cards = [];
-   initializeGame();
+  document.querySelector('#game').innerHTML = '';
+  cards = [];
+  initializeGame();
 }
 
 document.querySelector('#resetButton').addEventListener('click', resetGame);
 
 initializeGame();
-
-
